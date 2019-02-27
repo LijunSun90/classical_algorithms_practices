c4787c7cfa41b257855d67032f9cd0f1e8ae2843
LijunSun90 <sunlijun522@126.com>
2019-02-27 16:33:24 +0800

    add reverse string

---
+++ reverseString/build/CMakeFiles/reverseString.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reverseString.dir/reverseString.c.o"
+  "reverseString.pdb"
+  "reverseString"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/reverseString.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ reverseString/reverseString.c
@@ -0,0 +1,40 @@
+/*
+* Reverse a string. For example,
+* Input: "I have a pen."
+* Output: ".nep a evah I"
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int readString(char *userinput)
+{
+    printf("Type a sentence: ");
+    scanf("%[^\n]", userinput);
+    return 1;
+}
+
+int reverseIt(char *buf, char **reversed)
+{
+    int stringlen = strlen(buf), stringidx;
+    *reversed = (char *)malloc(stringlen + 1);
+    for(stringidx = 0; stringidx < stringlen; stringidx++){
+        (*reversed)[stringidx] = buf[stringlen - 1 - stringidx];
+    }
+    (*reversed)[stringlen] = '\0';
+
+    return 1;
+}
+
+int main(int argc, char *arg[])
+{
+    char userinput[100];
+    char *reversed;
+
+    readString(userinput);
+    reverseIt(userinput, &reversed);
+    printf("The reversed sentence is: %s\n", reversed);
+
+    return 0;
+}
